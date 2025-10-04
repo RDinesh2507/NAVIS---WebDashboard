785894edc26ac712ccba2f4921919102daffd7c8
Dinesh Ranganathan <rdinesh250704@gmail.com>
2025-10-04 19:16:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import datetime
 import random
+import os
 from flask import Flask, render_template
 
 # Initialize the Flask application
@@ -54,6 +55,6 @@ def dashboard():
     return render_template('index.html', data=data)
 
 if __name__ == '__main__':
-    # Setting debug=True allows the server to automatically reload on code changes
-    # and provides helpful error messages.
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Needed for Render.com
+    app.run(host='0.0.0.0', port=port)
+
